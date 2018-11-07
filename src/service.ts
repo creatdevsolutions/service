99b6d130a1bbb2ad0714418431293139b50578c4
Johann Wagner <wagner@creatdev-solutions.com>
2018-11-07 14:59:33 +0100

    Fixed import

--- src/service.ts
+++ src/service.ts
@@ -4,7 +4,7 @@
 import {Ping} from "./ping";
 
 // @ts-ignore
-import bundesstrasse from "@creatdevsolutions/bundesstrasse";
+import * as bundesstrasse from "@creatdevsolutions/bundesstrasse";
 import {BundesstrasseConfiguration, ChallengeExtra, Procedure, ServiceConfiguration} from "./types/types";
 
 const Logger = require("logplease");
